0c6762507576aab82a7a2bfaeab1f86a8de754f6
TestProjet <TestProjet@users.noreply.github.com>
2021-06-12 21:19:49 +0200

    Essai Niveau 1

--- GMTK_CA_JAM/Assets/Scripts/CameraFollow.cs
+++ GMTK_CA_JAM/Assets/Scripts/CameraFollow.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform targetToFollow;
+    [SerializeField] private float offsetCamera=3f;
 
     private void Update()
     {
-        transform.position = new Vector3(0, targetToFollow.position.y, transform.position.z);
+        transform.position = new Vector3(0, targetToFollow.position.y+offsetCamera, transform.position.z);
 
         if (shakeTimeRemaining > 0 && isShaking) GlobalShake();
     }
@@ -43,11 +44,11 @@ public class CameraFollow : MonoBehaviour
     {
         shakeTimeRemaining -= Time.deltaTime;
 
-        float x = Random.Range(-1f, 1f) * shakePower;
+        //float x = Random.Range(-1f, 1f) * shakePower;
         float y = Random.Range(0.3f,1f) * shakePower;
 
         transform.position = transform.position + new Vector3(0, y);
-        transform.rotation = Quaternion.Euler(0f, 0f, shakeRotation * Random.Range(-1f, 1f));
+        //transform.rotation = Quaternion.Euler(0f, 0f, shakeRotation * Random.Range(-1f, 1f));
 
         //shakeRotation = Mathf.MoveTowards(shakeRotation, 0f, shakeFadeTime * rotationMultiplier * Time.deltaTime);
 
