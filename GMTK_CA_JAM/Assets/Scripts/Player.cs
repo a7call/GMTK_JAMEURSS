040774da8fd96221f83d6b21925711902a13f65e
Aeroxal <69767755+Aeroxal@users.noreply.github.com>
2021-06-12 09:54:17 +0200

    Players

---
+++ GMTK_CA_JAM/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    [SerializeField] private KeyCode rightKey;
+    [SerializeField] private KeyCode leftKey;
+    [SerializeField] private float speed;
+    private float movement;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();    
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(rightKey)) movement = speed;
+        else if (Input.GetKey(leftKey)) movement = -speed;
+        else movement = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movement, 0);
+    }
+}
