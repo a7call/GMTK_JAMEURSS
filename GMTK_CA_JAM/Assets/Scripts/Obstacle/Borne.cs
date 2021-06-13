3d3f13baf0d05794deb0197cfd6686bbe62f1234
Aeroxal <69767755+Aeroxal@users.noreply.github.com>
2021-06-13 14:08:24 +0200

    Borne

--- GMTK_CA_JAM/Assets/Scripts/Obstacle/Borne.cs
+++ GMTK_CA_JAM/Assets/Scripts/Obstacle/Borne.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Borne : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator animator;
+
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("MainCamera"))
+        {
+            animator.SetTrigger("isCheck");
+        }
     }
+
 }
